951e5984c1e89ee5a9fdca1e7d518804b48fa58d
Jai Paragas <iaj.paragas@yahoo.com>
2025-06-09 11:17:44 +0800

    Remove project directories before adding as submodules

--- APIs/InventoryService/Features/Inventories/Contracts/Inventory.cs
+++
@@ -1,9 +0,0 @@
-namespace InventoryService.Features.Inventories.Contracts;
-
-/// <summary>
-/// Represents an inventory item.
-/// </summary>
-public record Inventory(
-    Guid ProductId,
-    int Quantity
-);
--- APIs/ProductCatalogService/Features/Products/Commands/DeleteProduct.cs
+++
@@ -1,8 +0,0 @@
-namespace ProductCatalogService.Features.Products.Commands;
-
-/// <summary>
-/// Represents a command to delete a product by its ID.
-/// </summary>
-public record DeleteProductCommand(
-    Guid Id
-);
